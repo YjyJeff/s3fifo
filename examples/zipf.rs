6c00f9fdb03dc154b204ceb6ee09b3efcb631f8f
YjyJeff <1731939194@qq.com>
2024-10-09 19:49:59 +0800

    add quick_cache and foryer for comparison

--- examples/zipf.rs
+++ examples/zipf.rs
@@ -2,11 +2,12 @@
 
 use rand::Rng;
 use s3fifo::S3FIFO;
+use std::time::Duration;
 
 fn main() {
     let upper_bound: u64 = 1_000_000;
     let cache_cap = upper_bound as usize / 100;
-    let zipf_distr = rand_distr::Zipf::new(upper_bound, 0.99).unwrap();
+    let zipf_distr = rand_distr::Zipf::new(upper_bound, 0.75).unwrap();
     let rng = rand::thread_rng();
     let mut iter = rng.sample_iter(zipf_distr);
 
@@ -17,36 +18,87 @@ fn main() {
         .collect::<Vec<_>>();
 
     let now = std::time::Instant::now();
+    let mut get_time = Duration::default();
+    let mut put_time = Duration::default();
     let mut hit_count = 0;
-    for &key in keys.iter() {
-        if cache.get(&key).is_some() {
+    for key in keys.iter() {
+        let now = std::time::Instant::now();
+        let entry = cache.get(key);
+        get_time += now.elapsed();
+
+        if entry.is_some() {
             hit_count += 1;
         } else {
-            cache.put(key, ());
+            let now = std::time::Instant::now();
+            cache.put(*key, ());
+            put_time += now.elapsed();
         }
     }
 
     println!(
-        "S3FIFO elapsed: {:?}. hit ratio: {}",
+        "S3FIFO elapsed: {:?}. hit ratio: {}. Get time: {:?}. Put time: {:?}",
         now.elapsed(),
-        hit_count as f64 / upper_bound as f64
+        hit_count as f64 / upper_bound as f64,
+        get_time,
+        put_time
     );
 
-    // LRU
+    // // LRU
+    // let mut hit_count = 0;
+    // let cache = Mutex::new(lru::LruCache::new(
+    //     std::num::NonZeroUsize::new(cache_cap).unwrap(),
+    // ));
+    // let now = std::time::Instant::now();
+    // for &key in keys.iter() {
+    //     if cache.lock().get(&key).is_some() {
+    //         hit_count += 1;
+    //     } else {
+    //         cache.lock().put(key, ());
+    //     }
+    // }
+    // println!(
+    //     "LRU elapsed: {:?}. hit ratio: {}",
+    //     now.elapsed(),
+    //     hit_count as f64 / upper_bound as f64
+    // );
+
+    // QuickCache
+    let mut cache = quick_cache::unsync::Cache::new(cache_cap);
+
     let mut hit_count = 0;
-    let mut cache = lru::LruCache::new(std::num::NonZeroUsize::new(cache_cap).unwrap());
     let now = std::time::Instant::now();
     for &key in keys.iter() {
         if cache.get(&key).is_some() {
             hit_count += 1;
         } else {
-            cache.put(key, ());
+            cache.insert(key, ());
         }
     }
 
     println!(
-        "LRU elapsed: {:?}. hit ratio: {}",
+        "QuickCache elapsed: {:?}. hit ratio: {}",
         now.elapsed(),
         hit_count as f64 / upper_bound as f64
     );
+
+    // // Foyer
+    // let cache = foyer::CacheBuilder::new(cache_cap)
+    //     .with_eviction_config(foyer::EvictionConfig::S3Fifo(foyer::S3FifoConfig::default()))
+    //     .build();
+
+    // let mut hit_count = 0;
+    // let now = std::time::Instant::now();
+    // for &key in keys.iter() {
+    //     if cache.get(&key).is_some() {
+    //         hit_count += 1;
+    //     } else {
+    //         cache.insert(key, ());
+    //     }
+    // }
+
+    // println!(
+    //     "Foyer elapsed: {:?}. hit ratio: {}",
+    //     now.elapsed(),
+    //     hit_count as f64 / upper_bound as f64
+    // );
 }
